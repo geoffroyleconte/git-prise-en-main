8080bb451d422c5e3a6c9650b0901924cf3e8311
Guillaume <g.thibault@polymtl.ca>
2023-01-10 17:37:21 -0500

    Adding simple solution

--- petit-exercice.jl
+++ petit-exercice.jl
@@ -1,16 +1,15 @@
 using LinearAlgebra
 
-A = [1.0 2.0 3.0;
+A = [1.0 2.0 3.0
     2.0 -5.0 3.0
     3.0 3.5 -4.0]
 b = ones(3)
 
 # 1) résoudre Ax = b 
-
 ### votre code ici
-# x = ...
-###
+x = inv(A) * b
 
+###
 # vérification
 # si besoin installer le module Test avec:
 # ]add Test 
