97ec681fad3fbfd250938a2c498144ca97c3f5f2
Fleu33 <92494465+Fleu33@users.noreply.github.com>
2023-01-11 10:12:11 -0500

    Update petit-exercice.jl
    Ajout de la résolution pour trouver x

--- petit-exercice.jl
+++ petit-exercice.jl
@@ -2,17 +2,17 @@ using LinearAlgebra
 
 A = [1.0 2.0 3.0;
     2.0 -5.0 3.0
-    3.0 3.5 -4.0]
-b = ones(3)
+    3.0 3.5 -4.0];
+b = ones(3);
 
 # 1) résoudre Ax = b 
 
 ### votre code ici
-# x = ...
+x = A \ b;
 ###
 
 # vérification
 # si besoin installer le module Test avec:
 # ]add Test 
 using Test
-@test norm(A * x - b) ≤ sqrt(eps())
+@test norm(A * x - b) ≤ sqrt(eps());
